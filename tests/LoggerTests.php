6a6d2d013c38793f4aea50fe23876b11aa0b3957
Fabian Stiehle <f.stiehle@gmail.com>
2016-05-03 15:31:03 +0200

    updated tests

--- tests/LoggerTests.php
+++ tests/LoggerTests.php
@@ -1,23 +1,56 @@
 <?php
 
 class LoggerTests extends PHPUnit_Framework_TestCase {
-    // Make sure core functionality is working as expected
-
+    /**
+     * Make sure core functionality is working as expected
+     * this is not, by all means, meant to cover the entire code
+     * Utilizes php output buffer to fetch echo output
+     * see: http://php.net/manual/de/function.ob-get-clean.php
+     */
     public function testLog() {
-        // Arrange
         $l = new Logger();
-        
-        $l->setLevel(Logger::INFO);        
-        $this->assertNotNull($l->info("test"));
-        $this->assertNotNull("test<br />", $l->warning("test"));        
-        $this->assertNotNull("test<br />", $l->critical("test"));
-        
-        $l->setLevel(Logger::CRITICAL);
-        $this->assertNull(null, $l->info("test"));
-        $this->assertNull(null, $l->warning("test"));   
+
+        $l->setLevel(Logger::INFO);  
+
+        ob_start();
+        $l->info("test");
+        $this->assertNotFalse(ob_get_clean());
+
+        ob_start();
+        $l->warning("test");
+        $this->assertNotFalse(ob_get_clean());
+
+        ob_start();
+        $l->critical("test");
+        $this->assertNotFalse(ob_get_clean());
 
         $l = new Logger("file.txt");
-        $this->assertNotNull(file_exists("file.txt"));
+        $l->info("test"); // log is created for first log entry
+        $this->assertTrue(file_exists("file.txt"));
+    }
+    
+    public function testOutput() {
+        $l = new Logger();
+        $l->critical("test");
+        $this->expectOutputRegex("/(\[.{0,}\b\]\[.{0,}\b\]\w{0,}: test)/");
+    }
+
+    public function testLevel() {
+        $l = new Logger();
+
+        $l->setLevel(Logger::CRITICAL); 
+
+        ob_start();
+        $l->info("test");
+        $this->assertEquals("", ob_get_clean());  
+
+        ob_start();
+        $l->warning("test");
+        $this->assertEquals("", ob_get_clean());
+
+        ob_start();
+        $l->critical("test");
+        $this->assertNotFalse(ob_get_clean());
     }
 
 }
